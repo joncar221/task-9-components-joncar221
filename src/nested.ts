084be444e1d4a154d6a12e55950a8da2215c9b78
Jonathan Carney <joncar@udel.edu>
2025-03-03 09:53:54 -0500

    finished sumPublishedPoints

--- src/nested.ts
+++ src/nested.ts
@@ -82,7 +82,12 @@ export function sumPoints(questions: Question[]): number {
  * Consumes an array of questions and returns the sum total of the PUBLISHED questions.
  */
 export function sumPublishedPoints(questions: Question[]): number {
-    return 0;
+    const result = questions.reduce(
+        (sum: number, question: Question): number =>
+            question.published ? sum + question.points : sum,
+        0,
+    );
+    return result;
 }
 
 /***
