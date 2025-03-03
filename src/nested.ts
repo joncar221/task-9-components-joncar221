eda1a28216a2fad1ab2281944838844262aad0f1
Jonathan Carney <joncar@udel.edu>
2025-03-03 09:26:47 -0500

    finished getNonEmptyQuestions

--- src/nested.ts
+++ src/nested.ts
@@ -18,7 +18,15 @@ export function getPublishedQuestions(questions: Question[]): Question[] {
  * `expected`, and an empty array for its `options`.
  */
 export function getNonEmptyQuestions(questions: Question[]): Question[] {
-    return [];
+    const result: Question[] = questions
+        .filter(
+            (question: Question): boolean =>
+                question.body.length > 0 ||
+                question.expected.length > 0 ||
+                question.options.length > 0,
+        )
+        .map((question: Question): Question => ({ ...question }));
+    return result;
 }
 
 /***
